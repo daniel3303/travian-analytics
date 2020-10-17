9731a4650e88dcf3279d48104de528f9ee3652cf
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2020-10-17 21:53:01 +0100

    Updated client.js version to 1.0.1

--- wwwroot/client.js
+++ wwwroot/client.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Travian - Analytics
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Análise de adversários.
 // @author       Daniel Oliveira
 // @match        https://*.travian.com/*
